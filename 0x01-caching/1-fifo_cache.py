78ce7be3d7e54e3886751f28429985394bb0d743
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-26 13:18:00 +0300

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,18 @@
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        self.cache_data = OrderedDict()
+        return super().__init__()
+    def put(self, key, item):
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            keys = list(self.cache_data.keys())
+            self.cache_data.pop(keys[0])
+            print(f"DISCARD: {keys[0]}")
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        return self.cache_data.get(key, None)
