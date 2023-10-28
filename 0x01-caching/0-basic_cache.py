47944124090cd1aa38c4e69a52b82a249265fb22
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-28 20:54:37 +0300

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+A basic caching implementation that stores key-value pairs
+in memory.
+"""
 from base_caching import BaseCaching
 
 
@@ -11,9 +15,16 @@ class BasicCache(BaseCaching):
     implements the put and get methods.
     """
     def put(self, key, item):
+        """
+        Adds key-value pair to the cache data.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Retrieves the item associated with key from the
+        cache data.
+        """
         return self.cache_data.get(key, None)
